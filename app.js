02787c7b9c635f6919d282897e6df3237d6458fa
David McDeavitt <davidmcdeavitt@Davids-MacBook-Air.local>
2017-06-14 08:06:23 +1000

    update for css and parent and now no x

--- app.js
+++ app.js
@@ -5,9 +5,23 @@ var onlySolution = "X"
 // first give the div an ability to seen
 
 var pickMe = document.querySelector('div')
+//add the input square
+var squareBox = document.querySelector('tile')
 
+//need to create the parent reference
+var parent = document.getElementsByClassName('parent')[0]
+
+
+
+
+var logParent = function (event) {
+  console.log(event.target.value);
+  return pickMe.value = "X";
+}
+
+parent.addEventListener('click', logParent)
 // not give the element an abilitity to be clicked
-pickMe.addEventListener('click', function() {
-
-  console.log(onlySolution);;
-})
+// pickMe.addEventListener('click', function() {
+//   squareBox.textContent = "";
+//   return squareBox.value = onlySolution;
+// })
