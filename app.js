9ee0b35e30cda0ac891739eb9086ac2998837471
David McDeavitt <davidmcdeavitt@192-168-1-5.tpgi.com.au>
2017-06-16 07:06:42 +1000

    add for functionality

--- app.js
+++ app.js
@@ -45,12 +45,12 @@ var board = '         '.split('')
 //board[0] = x
 // if board[0, 4, 9] === 'x' then winner
 var switchPlayer
-
+var stepladder;
 var playerOne = 'X'
 
 var playerTwo = 'O'
 var currentPlayer = playerOne
-
+var downLadder;
 
 
 
@@ -69,10 +69,16 @@ var currentPlayer = playerOne
 var playGame = function(event) {
   //put three comments make move,
   event.target.textContent = currentPlayer;
-  board [] = event.target.getAttribute('data-index');
-//board [] =
-//check win,
-  // runWinner()
+  // var dataIndex = event.target.getElementById('textArea')
+  var dataIndex = +event.target.getAttribute('data-index');
+  board[dataIndex] = currentPlayer
+
+
+//creates the basis for stepladder
+
+
+// check win,
+  runWinner(event)
 
   //switch turn
   if (currentPlayer === playerOne) {
@@ -109,37 +115,183 @@ var playGame = function(event) {
 
 
 
+
 var runWinner = function(event) {
-  if (board[0] === 'X' &&
-      board[1] === 'X' &&
-      board[2] === 'X') {
-      console.log("Player One Wins!");
+
+  //for all X functions
+  //diagonally up
+  stepLadder = board.slice(2,7).map(function(letter, index){
+      if (letter === "X") {
+          return letter}
+  })
+  if (stepLadder.length > 3) {
+    trashBucket = stepLadder.splice(1,1);
+    trashBucket = stepLadder.splice(2,1);
+  }
+
+  //diagonally down
+  downLadder = board.slice(0,9).map(function(letter, index){
+          if (letter === "X") {
+            return letter
+          }
+  })
+  if (downLadder.length > 3) {
+    trashBucket = downLadder.splice(1,3);
+    trashBucket = downLadder.splice(2,3);
   }
 
 
-  else if(document.querySelector('.index0').value === 'X' &&
-      document.querySelector('.index3').value === 'X' &&
-      document.querySelector('.index6').value === 'X') {
-      console.log("Player One Wins!");
-  }else if(document.querySelector('.index2').value === 'X' &&
-      document.querySelector('.index4').value === 'X' &&
-      document.querySelector('.index6').value === 'X') {
-      console.log("Player One Wins!");
-  }else if(document.querySelector('.index3').value === 'X' &&
-      document.querySelector('.index4').value === 'X' &&
-      document.querySelector('.index5').value === 'X') {
-      console.log("Player One Wins!");
-  }else if(document.querySelector('.index6').value === 'X' &&
-      document.querySelector('.index7').value === 'X' &&
-      document.querySelector('.index8').value === 'X') {
-      console.log("Player One Wins!");
-  }else if(document.querySelector('.index2').value === 'X' &&
-      document.querySelector('.index4').value === 'X' &&
-      document.querySelector('.index6').value === 'X') {
-      console.log("Player One Wins!");
+
+  leftColumn = board.slice(0,9).map(function(letter, index){
+      if (letter === "X") {
+          return letter
+
+      }
+  })
+  if (leftColumn.length > 3) {
+    trashBucket = leftColumn.splice(1,2);
+    trashBucket = leftColumn.splice(2,2);
+    trashBucket = leftColumn.splice(3,2);
   }
 
-}
+  centerColumn = board.slice(0,9).map(function(letter, index){
+      if (letter === "X") {
+          return letter
+
+      }
+  })
+  if (centerColumn.length > 3) {
+    trashBucket = centerColumn.splice(0,1);
+    trashBucket = centerColumn.splice(1,2);
+    trashBucket = centerColumn.splice(2,2);
+    trashBucket = centerColumn.splice(3,1);
+  }
+
+  rightColumn = board.slice(0,9).map(function(letter, index){
+      if (letter === "X") {
+          return letter
+      }
+  })
+  if (rightColumn.length > 3) {
+    trashBucket = rightColumn.splice(0,2);
+    trashBucket = rightColumn.splice(1,2);
+    trashBucket = rightColumn.splice(2,2);
+  }
+
+  OstepLadder = board.slice(2,7).map(function(letter, index){
+      if (letter === "O") {
+          return letter
+      }
+  })
+  if (OstepLadder.length > 3) {
+    trashBucket = OstepLadder.splice(1,1);
+    trashBucket = OstepLadder.splice(2,1);
+  }
+
+  //diagonally down
+  OdownLadder = board.slice(0,9).map(function(letter, index){
+      if (letter === "O") {
+          return letter
+      }
+  })
+  if (OdownLadder.length > 3) {
+    trashBucket = OdownLadder.splice(1,3);
+    trashBucket = OdownLadder.splice(2,3);
+  }
+
+
+  OleftColumn = board.slice(0,9).map(function(letter, index){
+      if (letter === "O") {
+          return letter
+      }
+  })
+  if (OleftColumn.length > 3) {
+    trashBucket = OleftColumn.splice(1,2);
+    trashBucket = OleftColumn.splice(2,2);
+    trashBucket = OleftColumn.splice(3,2);
+  }
+
+  OcenterColumn = board.slice(0,9).map(function(letter, index){
+      if (letter === "O") {
+          return letter
+      }
+  })
+  if (OcenterColumn.length > 3) {
+    trashBucket = OcenterColumn.splice(0,1);
+    trashBucket = OcenterColumn.splice(1,2);
+    trashBucket = OcenterColumn.splice(2,2);
+    trashBucket = OcenterColumn.splice(3,1);
+  }
+
+  OrightColumn = board.slice(0,9).map(function(letter, index){
+      if (letter === "O") {
+          return letter
+      }
+  })
+  if (OrightColumn.length > 3) {
+    trashBucket = OrightColumn.splice(0,2);
+    trashBucket = OrightColumn.splice(1,2);
+    trashBucket = OrightColumn.splice(2,2);
+  }
+
+
+//horizontal tests
+  if (board.slice(0,3).join('') === "XXX" ||
+      board.slice(3,6).join('') === "XXX" ||
+      board.slice(6,9).join('') === "XXX" ||
+      //diagonal tests
+      stepLadder.join('') === "XXX"       ||
+      downLadder.join('') === "XXX"       ||
+      //verticle tests
+      leftColumn.join('') === "XXX"       ||
+      centerColumn.join('') === "XXX"     ||
+      rightColumn.join('') === "XXX"
+    ) {
+      console.log("Player One Wins!");
+  }
+  else if(
+      board.slice(0,3).join('') === "OOO" ||
+      board.slice(3,6).join('') === "OOO" ||
+      board.slice(6,9).join('') === "OOO" ||
+      stepLadder.join('') === "OOO"       ||
+      downLadder.join('') === "OOO"       ||
+      //verticle tests
+      leftColumn.join('') === "OOO"       ||
+      centerColumn.join('') === "OOO"     ||
+      rightColumn.join('') === "OOO"
+    ) {
+    console.log("Player Two Wins!!!");
+  }
+  else {
+      console.log('Draw');
+  }
+
+};
+
+//
+//   else if(document.querySelector('.index0').value === 'X' &&
+//       document.querySelector('.index3').value === 'X' &&
+//       document.querySelector('.index6').value === 'X') {
+//       console.log("Player One Wins!");
+//   }else if(document.querySelector('.index2').value === 'X' &&
+//       document.querySelector('.index4').value === 'X' &&
+//       document.querySelector('.index6').value === 'X') {
+//       console.log("Player One Wins!");
+//   }else if(document.querySelector('.index3').value === 'X' &&
+//       document.querySelector('.index4').value === 'X' &&
+//       document.querySelector('.index5').value === 'X') {
+//       console.log("Player One Wins!");
+//   }else if(document.querySelector('.index6').value === 'X' &&
+//       document.querySelector('.index7').value === 'X' &&
+//       document.querySelector('.index8').value === 'X') {
+//       console.log("Player One Wins!");
+//   }else if(document.querySelector('.index2').value === 'X' &&
+//       document.querySelector('.index4').value === 'X' &&
+//       document.querySelector('.index6').value === 'X') {
+//       console.log("Player One Wins!");
+//   }
+//
+// }
 
 //
 // parent.addEventListener('click', runWinner);
